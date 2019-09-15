4e9cc97e5130057fc54885f01cb237ea792789df
Daiki <nojiri1098@gmail.com>
2019-09-15 12:24:31 +0900

    Add video.playWithDetection()

--- video.py
+++ video.py
@@ -1,11 +1,15 @@
 # -*- coding: UTF-8 -*-
 
 import cv2
+import os
 import const
 import util.image as image
 
+TYPE_DETECTION = 10
+
 class Video:
     def __init__(self, path):
+        self.file_name = os.path.basename(path)
         self.video = self.open(path)
         self.current_color = self.__getNextFrame()
         self.current_gray = image.cvt2Gray(self.current_color)
@@ -14,9 +18,19 @@ class Video:
     def open(self, path):
         return cv2.VideoCapture(path)
 
-    def play(self):
+    def play(self, type=None):
         while(self.current_color is not None):
-            cv2.imshow('result', video.current_color)
+
+            if type is TYPE_DETECTION:
+                threshold = image.subtract(self.before_gray, self.current_gray)
+                contours, heirarchy = image.findContours(threshold)
+
+                for contour in contours:
+                    if const.MIN_AREA < cv2.contourArea(contour) < const.MAX_AREA:
+                        x, y, w, h = cv2.boundingRect(contour)
+                        cv2.rectangle(self.current_color, (x, y), (x+w, y+h), const.RECT_COLOR_TRACK, 2)
+
+            cv2.imshow(self.file_name, self.current_color)
 
             if cv2.waitKey(1) == ord('q'):
                 break
@@ -24,6 +38,9 @@ class Video:
             self.moveToNextFrame()
 
         cv2.destroyAllWindows()
+
+    def playWithDetection(self):
+        self.play(type=TYPE_DETECTION)
 
     def close(self):
         self.video.release()
@@ -38,5 +55,5 @@ class Video:
 
 if __name__ == "__main__":
     video = Video(const.VIDEO_PATH)
-    video.play()
+    video.playWithDetection()
     video.close()
