ebae16298711d322e26b3febc7cb9fcd1e5a5867
Daiki <nojiri1098@gmail.com>
2019-10-10 09:02:20 +0900

    Reduce functions for video
    検出したオブジェクトを画像として保存するかどうかを選択できれば十分

--- study/video.py
+++ study/video.py
@@ -6,12 +6,6 @@ from study import const
 from study.object import Object
 import study.util.image as image
 
-TYPE_PALY = 0
-TYPE_DETECTION = 10
-TYPE_DISPLAY = 20
-TYPE_SELECTION = 30
-TYPE_ALL_SAVING = 40
-
 
 class Video:
     def __init__(self, path):
@@ -24,65 +18,50 @@ class Video:
     def open(self, path):
         return cv2.VideoCapture(path)
 
-    def play(self, type=TYPE_PALY, start_from=0):
+    def play(self, save=False, start_from=0):
         for i in range(start_from):
             self.moveToNextFrame()
 
         classifier = cv2.CascadeClassifier(const.CASCADE_PATH)
 
+        if save is True:
+            image_path = os.path.join('image', self.file_name)
+            if not os.path.exists(image_path):
+                os.makedirs(image_path, exist_ok=True)
+
         cnt = 1
         while self.current_color is not None:
 
-            if type >= TYPE_DETECTION:
+            objects = classifier.detectMultiScale(
+                self.current_color,
+                scaleFactor=1.05,
+                minNeighbors=2,
+                minSize=(10, 10),
+            )
 
-                objects = classifier.detectMultiScale(
+            for object in objects:
+                (x, y) = tuple(object[0:2])
+                (w, h) = tuple(object[2:4])
+
+                if save is True:
+                    cv2.imwrite(os.path.join(image_path, '{0}.png'.format(cnt)), self.current_gray[y : y + h, x : x + w])
+                    cnt += 1
+
+                cv2.rectangle(
                     self.current_color,
-                    scaleFactor=1.05,
-                    minNeighbors=2,
-                    minSize=(10, 10),
+                    (x, y),
+                    (x + w, y + h),
+                    const.RECT_COLOR_TRUCK,
+                    2,
                 )
-
-                for object in objects:
-                    (x, y) = tuple(object[0:2])
-                    (w, h) = tuple(object[2:4])
-
-                    if type >= TYPE_DISPLAY:
-                        object = Object(x, y, w, h, self.current_gray[y : y + h, x : x + w])
-                        image.show('trimmed', object.image, gray=True)
-
-                    if type >= TYPE_SELECTION:
-                        k = cv2.waitKey(0) & 0xFF
-                        if k == ord('t') or k == ord('c'):
-                            cv2.imwrite(
-                                'image/{0}_{1}.png'.format(cnt, chr(k)), object.image
-                            )
-                            cnt += 1
-
-                    cv2.rectangle(
-                        self.current_color,
-                        (x, y),
-                        (x + w, y + h),
-                        const.RECT_COLOR_TRUCK,
-                        2,
-                    )
-
-            cv2.imshow(self.file_name, self.current_color)
 
             if cv2.waitKey(1) == ord('q'):
                 break
 
+            self.showFrame()
             self.moveToNextFrame()
 
         cv2.destroyAllWindows()
-
-    def playWithDetection(self, start_from=0):
-        self.play(type=TYPE_DETECTION, start_from=start_from)
-
-    def playWithDisplay(self, start_from=0):
-        self.play(type=TYPE_DISPLAY, start_from=start_from)
-
-    def playWithSelection(self, start_from=0):
-        self.play(type=TYPE_SELECTION, start_from=start_from)
 
     def close(self):
         self.video.release()
@@ -95,8 +74,10 @@ class Video:
         self.current_color = self.__getNextFrame()
         self.current_gray = image.cvt2Gray(self.current_color)
 
+    def showFrame(self):
+        cv2.imshow(self.file_name, self.current_color)
 
 def main():
     video = Video(const.VIDEO_PATH)
-    video.playWithSelection(start_from=1000)
+    video.play(save=True)
     video.close()
