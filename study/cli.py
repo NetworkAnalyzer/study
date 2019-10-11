c91a0771a23d8fec5dfc4f01edcaec23253d8657
Daiki <nojiri1098@gmail.com>
2019-10-11 09:40:01 +0900

    Using dataset_path instead of base_path

--- study/cli.py
+++ study/cli.py
@@ -1,6 +1,6 @@
 import click
 from . import make_dataset, image, video, const, anfis, logger
-from .util.path import base_path
+from .util.path import base_path, dataset_path
 
 @click.group("cli")
 @click.option("--log", default="INFO", help="Logging level of the logger.")
@@ -35,8 +35,8 @@ def constant():
 def anf(dataset):
 
     dataset_paths = {
-        'car'   : base_path('dataset/dataset_{0}_for_c.csv'.format(dataset)),
-        'truck' : base_path('dataset/dataset_{0}_for_t.csv'.format(dataset))
+        'car'   : dataset_path('dataset_{0}_for_c.csv'.format(dataset)),
+        'truck' : dataset_path('dataset_{0}_for_t.csv'.format(dataset))
     }
 
     click.echo('use {0}'.format(dataset_paths['car']))
