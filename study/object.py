bbad7d343bf4a8c0deb119ec41c3e4fdf0d7032f
Daiki <nojiri1098@gmail.com>
2019-10-16 14:49:21 +0900

    Fix name error

--- study/object.py
+++ study/object.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-import study.util.image as image
+import study.util.image as util_image
 from skimage.feature import greycomatrix, greycoprops
 
 
@@ -9,7 +9,7 @@ class Object:
         self.glcm = self.__glcm(image)
 
     def __glcm(self, image):
-        grey = image.cvt2Gray(image)
+        grey = util_image.cvt2Gray(image)
         return greycomatrix(grey, [1], [0])
 
     def get(self, feature_name):
