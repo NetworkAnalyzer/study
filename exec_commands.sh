a3460ad439994aab8472ad985a025a3d40255994
Daiki <nojiri1098@gmail.com>
2019-10-30 12:13:30 +0900

    Update make_commands.sh

--- exec_commands.sh
+++ exec_commands.sh
@@ -3,5 +3,6 @@
 cat commands | while read line
 do
   $line >> accuracy
+  echo "${line} is done"
 done
 
--- make_commands.sh
+++ make_commands.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm commands
+
 bash permutation_combination.sh 5 3 c | while read line
 do
   line=${line/1/contrast}
@@ -8,6 +10,9 @@ do
   line=${line/4/asm}
   line=${line/5/correlation}
 
+  echo "study anfis --epochs=20 --video=1 --feature=${line}" >> commands
+  echo "study anfis --epochs=20 --video=2 --feature=${line}" >> commands
+  echo "study anfis --epochs=20 --video=3 --feature=${line}" >> commands
   echo "study anfis --epochs=20 --video=4 --feature=${line}" >> commands
 done
 
