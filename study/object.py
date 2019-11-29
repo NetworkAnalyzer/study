20f2e927e889a3802ce3b931bd072ce7579cbc03
Daiki <nojiri1098@gmail.com>
2019-11-29 16:00:53 +0900

    Add feature extraction
    CompactnessとHWRを使った場合の性能を確認するために復活させた

--- study/object.py
+++ study/object.py
@@ -5,12 +5,33 @@ from skimage.feature import greycomatrix, greycoprops
 
 
 class Object:
-    def __init__(self, image):
+    def __init__(self, image, h=0, w=0):
+        self.compactness = self.__compactness(h, w)
+        self.hwr = self.__HWR(h, w)
         self.glcm = self.__glcm(image)
 
     def __glcm(self, image):
         grey = util_image.cvt2Gray(image)
         return greycomatrix(grey, [1], [0])
 
+    def __compactness(self, h, w):
+        area = h * w
+        prim = 2 * h + 2 * w
+
+        return float(area) / prim**2
+    
+    def __HWR(self, h, w):
+        return float(h) / w
+
     def get(self, features):
-        return [greycoprops(self.glcm, feature)[0][0] for feature in features]
+        ret = []
+
+        for feature in features:
+            if feature === 'compactness':
+                ret.append(self.compactness)
+            else if feature === 'hwr':
+                ret.append(self.hwr)
+            else:
+                ret.append(greycoprops(self.glcm, feature)[0][0])
+        
+        return ret
